c5c20288cda1917ba7b0dbbc2b0d5b1132cc8d64
Artem <duketemon@users.noreply.github.com>
2023-05-28 20:25:53 +0000

    Updated name of the unit test

--- tests/assets/test_fiat.py
+++ tests/assets/test_fiat.py
@@ -3,7 +3,7 @@ from typing import Iterable
 from currency_codes.assets.fiat import get_fiat_currencies
 
 
-def test_get_fiat_currencies() -> None:
+def test_get_fiat_currencies_when_function_returns_all_currencies() -> None:
     # When
     currencies = get_fiat_currencies()
 
